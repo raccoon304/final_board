55e770b063d56301478a1ada3df0394352f69092
kimminsoo12 <ansjdhsl@naver.com>
2026-03-27 19:06:18 +0900

    0327(확인용)

--- src/main/java/com/spring/app/community/controller/CommunityController.java
+++ src/main/java/com/spring/app/community/controller/CommunityController.java
@@ -21,7 +21,7 @@ import com.spring.app.community.service.CommunityService;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/community") /*4*/
+@RequestMapping("/community") /*5*/
 public class CommunityController {
 
     private final CommunityService communityService;
